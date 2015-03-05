e2e0b419b9cea714c04738aa8534210a003e1c7d
glathoud <glathoud@yahoo.fr>
2015-03-05 05:56:21 +0100

    learning D...

--- d/hello_world.d
+++ d/hello_world.d
@@ -1,4 +1,6 @@
+import std.conv;    // to!
 import std.stdio;
+import std.string;  // chomp
 
 void main()
 {
@@ -17,7 +19,40 @@ void main()
   TInfoNormal!( idouble ).print();
   TInfoNormal!( cdouble ).print();
 
-// xxx http://ddili.org/ders/d.en/input.html
+
+  // http://ddili.org/ders/d.en/arithmetic.html
+
+  // ubyte ub = to!ubyte(29999.0f); // Conversion positive overflow
+
+    uint number_1 = 10;
+    uint number_2 = 20;
+
+    writeln("PROBLEM! uint cannot have negative values:");
+    writeln(number_1 - number_2);
+    writeln(number_2 - number_1);
+    
+
+    writeln( 7 / 2 );
+    writeln( 7f / 2 );
+    writeln( 7.0 / 2 );
+
+    int nnn = 2;
+    nnn ^^= 6;  // elevate to the 6th power
+    writeln( "nnn ", nnn );  // now equal to 2^^6 == 64
+    
+
+    // block
+
+    int iii = 3;
+    writeln( "iii ", iii );
+    {
+      int iii = 4;
+      writeln( "iii in block scope ", iii );
+    }
+    
+    
+
+  // http://ddili.org/ders/d.en/input.html
 
     write("How many students are there? ");
     int studentCount;
@@ -30,7 +65,32 @@ void main()
     writeln("Got it: There are ", studentCount, " students",
             " and ", teacherCount, " teachers.");
 
+    write("Do you like that statement? ");
+    bool youlike;
+    youlike = read_bool();
+
+    if (youlike)
+      writeln( "You like it! (", youlike, ")" );
+    else
+      writeln( "You don't like it! (", youlike, ")" );
+
+  int number = 3;
+
+    while (number == 3) {
+        write("Number? ");
+        readf(" %s", &number);
+    }
 }
+
+bool read_bool()
+{
+  string input;
+  while (input.length == 0)
+    input = chomp( readln() );
+  
+  return to!bool( input );
+}
+
 
 void print_type_info_int( int v )  // ...would like to generalize, see below
 {

73561777279bbee7d66d6eb173da2f1e74caeed8
glathoud <glathoud@yahoo.fr>
2015-03-12 19:36:09 +0100

    D D D

--- d/hello_world.d
+++ d/hello_world.d
@@ -163,6 +163,190 @@ void main()
     writeln("The uppercase of ş: ", toUpper('ş'));
     writeln("The uppercase of ı: ", toUpper('ı'));
     
+    // http://ddili.org/ders/d.en/slices.html
+
+    int[7] toto = [0, 1, 2, 3, 4, 5, 6];
+    int[]  begin = toto[ 0..4 ];
+    int[]    end = toto [4..$];
+    
+    begin[ 0 ] = 77;
+    end[ 0 ]   = 88;
+    writeln( "toto ", toto );
+
+
+
+    int[12] monthDays =
+        [ 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 ];
+
+    int[] leapYear = monthDays.dup;
+
+    ++leapYear[1];   // increments the days in February
+
+    writeln("Non-leap year: ", monthDays);
+    writeln("Leap year    : ", leapYear);
+
+
+
+   int[3] a = [ 1, 1, 1 ];
+    int[3] b = [ 2, 2, 2 ];
+
+    a = b;        // the elements of 'a' become 2
+    writeln("a ", a);
+    writeln("b ", b);
+    writeln("a==b ", a==b);
+    writeln("a[0]=33;");
+    a[0]=33;
+    writeln("a ", a);
+    writeln("b ", b);
+    writeln("a==b ", a==b);
+    
+
+    // capacity
+
+    writeln();
+
+    int[] slice = [ 1, 3, 5, 7, 9, 11, 13, 15 ];
+    int[] half = slice[0 .. $ / 2];
+    int[] quarter = slice[0 .. $ / 4];
+
+    writeln("slice.capacity: ", slice.capacity, ", remaining: ", slice.capacity - slice.length );
+    writeln("half.capacity: ", half.capacity);
+    writeln("quarter.capacity: ", quarter.capacity);
+
+    slice ~= 42;      // adding to the longest slice ...
+    slice[1] = 0;     // ... and then modifying an element
+
+    writeln("slice.capacity: ", slice.capacity, ", remaining: ", slice.capacity - slice.length );
+    writeln("half.capacity: ", half.capacity);
+    writeln("quarter.capacity: ", quarter.capacity);
+
+    writeln("q h s");
+    writeln(quarter);
+    writeln(half);
+    writeln(slice);
+
+    writeln( "quarter ~= 123;" );
+
+    quarter ~= 123;
+
+    writeln("slice.capacity: ", slice.capacity, ", remaining: ", slice.capacity - slice.length );
+    writeln("half.capacity: ", half.capacity);
+    writeln("quarter.capacity: ", quarter.capacity);
+
+    writeln("q h s");
+    writeln(quarter);
+    writeln(half);
+    writeln(slice);
+
+    writeln( "\nquarter[0]=-3;" );
+    quarter[0]=-3;
+
+    writeln("slice.capacity: ", slice.capacity, ", remaining: ", slice.capacity - slice.length );
+    writeln("half.capacity: ", half.capacity);
+    writeln("quarter.capacity: ", quarter.capacity);
+
+    writeln("q h s");
+    writeln(quarter);
+    writeln(half);
+    writeln(slice);
+
+    writeln( "\nhalf[0]=-7;" );
+    half[0]=-7;
+
+    writeln("slice.capacity: ", slice.capacity, ", remaining: ", slice.capacity - slice.length );
+    writeln("half.capacity: ", half.capacity);
+    writeln("quarter.capacity: ", quarter.capacity);
+
+    writeln("q h s");
+    writeln(quarter);
+    writeln(half);
+    writeln(slice);
+
+    // Operations on all elements
+
+    writeln();
+    writeln("op on all elements");
+
+    double[] five = [0,1,2,3,4];
+    writeln("five (before): ", five);
+    five[] /= sum( five );
+    writeln("five (after): ", five);
+
+    writeln();
+    writeln("op on all elements (T)");
+
+    float[] six = [5,4,3,2,1,0];
+    writeln("six (before): ", six);
+    six[] /= TArr!(float).sum( six );
+    writeln("six (after): ", six);
+
+    
+    writeln();
+
+    double[] slice1 = [ 1, 1, 1 ];
+    double[] slice2 = [ 2, 2, 2 ];
+    double[] slice3 = [ 3, 3, 3 ];
+
+    slice2 = slice1;      // ← slice2 starts providing access
+                          //   to the same elements that
+                          //   slice1 provides access to
+
+    slice3[] = slice1;    // ← the values of the elements of
+                          //   slice3 change
+
+    writeln("slice1 before: ", slice1);
+    writeln("slice2 before: ", slice2);
+    writeln("slice3 before: ", slice3);
+
+    slice2[0] = 42;       // ← the value of an element that
+                          //   it shares with slice1 changes
+
+    slice3[1] = 43;       // ← the value of an element that
+                          //   only it provides access to
+                          //   changes
+
+    writeln("slice1 after : ", slice1);
+    writeln("slice2 after : ", slice2);
+    writeln("slice3 after : ", slice3);
+
+
+    writeln();
+    int[2][3][4] arr3d;
+    arr3d[0][2][1] = 3;
+    ++arr3d[1][0][0];
+    writeln( arr3d );
+
+
+    writeln();
+
+    double[] array_2h = [ 1, 20, 2, 30, 7, 11 ];
+    int i_2h = array_2h.length;
+    while (i_2h--)
+      if (array_2h[ i_2h ] >= 10)
+        array_2h[ i_2h ] /= 2;
+    
+    writeln( array_2h );
+    
+    // Same with slice
+
+    double[] array2_2h = [ 1, 20, 2, 30, 7, 11 ];    
+    double[] array2_slice = array2_2h;
+    
+    while (array2_slice.length)
+      {
+        if (array2_slice[0] >= 10)
+          array2_slice[ 0 ] /= 2;
+
+        array2_slice = array2_slice[ 1 .. $ ];
+      }
+    
+    writeln( array2_2h );
+
+
+     char[] sx = "hello".dup;
+    sx[0] = 'H';
+    writeln(sx);
+
     // ------- inputs -------
 
     // http://ddili.org/ders/d.en/floating_point.html
@@ -208,6 +392,28 @@ void main()
         write("Number? ");
         readf(" %s", &number);
     }
+}
+
+double sum( double[] arr )
+{
+  double sum = 0;
+  foreach ( x; arr )
+    sum += x;
+
+  return sum;
+}
+
+template TArr( T ) {
+
+  T sum( T[] arr )
+  {
+    T ret = 0;
+
+    foreach( x ; arr )
+      ret += x;
+
+    return ret;
+  }
 }
 
 bool read_bool()
